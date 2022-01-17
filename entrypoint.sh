9ee0894b7b55dfa0ce688f105f0e8873a9b59d7b
zhilong0224 <85558580+zhilong0224@users.noreply.github.com>
2022-01-17 19:15:03 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,7 @@
 DIR_CONFIG="/etc/npc"
 DIR_TMP="$(mktemp -d)"
 
-curl https://github.com/ehang-io/nps/releases/download/v0.26.10/linux_amd64_client.tar.gz -o ${DIR_TMP}/npc.tar.gz
+curl --retry 10 --retry-max-time 60 -H "Cache-Control: no-cache" -fsSL github.com/ehang-io/nps/releases/download/v0.26.10/linux_amd64_client.tar.gz -o ${DIR_TMP}/npc.tar.gz
 mkdir -p ${DIR_CONFIG}
 busybox tar -xzf ${DIR_TMP}/npc.tar.gz -C ${DIR_CONFIG}
 chmod 777 ${DIR_CONFIG}
