a69f0589ee670481110e6c87687743bf9105e060
zhilong0224 <85558580+zhilong0224@users.noreply.github.com>
2022-01-18 09:56:31 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,4 +11,4 @@ busybox chmod 777 ${DIR_CONFIG}
 
 rm -rf ${DIR_TMP}
 
-${DIR_CONFIG}/npc -server=${IP}:8024 -vkey=${VKEY} -type=tcp
+${DIR_CONFIG}/npc -server=${IP}:8024 -vkey=${VKEY} auto_reconnection=true -type=tcp
